c1249dd2f6aea6d0af727a5f97f35f604ae35ba6
Magnus Andersson <mpja69@gmail.com>
2024-08-28 10:53:37 +0200

    added frequency counter

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-with open("books/frankenstein.txt") as f:
-    file_content = f.read()
+def main():
+    book_path = "books/frankenstein.txt"
+    text = read_file(book_path)
+    count = count_words(text)
+    print(count)
+    freq = calculate_frequency(text)
+    print(freq)
 
+def read_file(path):
+    with open(path) as f:
+        file_content = f.read()
+    return file_content
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def calculate_frequency(text):
+    chars = {}
+    for ch in text:
+        lowered = ch.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+    
+main()
 
